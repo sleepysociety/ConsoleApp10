d977c2d073f6e9b11a13bfe168c5bc04820cfba8
sleepysociety <59642440+sleepysociety@users.noreply.github.com>
2020-01-09 13:37:31 +0100

    added dumb

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello dumb World!");
         }
     }
 }
